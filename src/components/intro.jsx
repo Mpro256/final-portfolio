a4a1af4eefb107f66459fbd55d12a96aa18603e3
czcx <106482372+Mpro256@users.noreply.github.com>
2024-03-03 08:24:32 -0500

    Update intro.jsx

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -14,7 +14,7 @@ class Intro extends React.Component {
         <div className="intro-content display-table">
           <div className="table-cell">
             <div className="container">
-              <h1 className="intro-title mb-4">Hello, I am XYZ</h1>
+              <h1 className="intro-title mb-4">Hello, My name is Benjamin Kim</h1>
               <p className="intro-subtitle">
                 <span className="text-slider-items"></span>
                 <strong className="text-slider">
@@ -22,9 +22,10 @@ class Intro extends React.Component {
                     strings={[
                       "Front End Developer",
                       "Back End Developer",
-                      "Software Engineer"
+                      "Software Engineer",
+                      "Minecraft Nerd"
                     ]}
-                    typeSpeed={80}
+                    typeSpeed={85}
                     backDelay={1100}
                     backSpeed={30}
                     loop
