413ed44184fb41f9684b0811ae7b6603e5585949
czcx <106482372+Mpro256@users.noreply.github.com>
2024-03-03 08:29:52 -0500

    Update about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,55 +6,55 @@ class About extends React.Component {
     super();
     this.state = {
       skills: [
-        { id: "HTML5_skill", content: "HTML5", porcentage: "80%", value: "80" },
-        { id: "CSS3_skill", content: "CSS3", porcentage: "75%", value: "75" },
+        { id: "HTML5_skill", content: "HTML5", porcentage: "95%", value: "95" },
+        { id: "CSS3_skill", content: "CSS3", porcentage: "55%", value: "55" },
         {
           id: "JavaScript_skill",
           content: "JavaScript",
-          porcentage: "90%",
-          value: "90"
+          porcentage: "100%",
+          value: "100"
         },
-        { id: "PHP_skill", content: "PHP", porcentage: "70%", value: "70" },
+        { id: "Java_Skill", content: "Java", porcentage: "85%", value: "85" },
         {
           id: "ReactJS_skill",
           content: "ReactJS",
-          porcentage: "80%",
-          value: "80"
+          porcentage: "65%",
+          value: "65"
         },
         {
           id: "Python_skill",
           content: "Python",
-          porcentage: "75%",
-          value: "75"
+          porcentage: "90%",
+          value: "90"
         },
         {
           id: "VanillaJS_skill",
           content: "VanillaJS",
-          porcentage: "85%",
-          value: "85"
+          porcentage: "15%",
+          value: "15"
         },
         {
           id: "Wordpress_skill",
           content: "Wordpress",
-          porcentage: "80%",
-          value: "80"
+          porcentage: "20%",
+          value: "20"
         }
       ],
       about_me: [
         {
           id: "first-p-about",
           content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+            "Hey there! My name is Ben. I made this website to tell you some more about me and my coding works."
         },
         {
           id: "second-p-about",
           content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+            "One of my biggest interests besides Web Development is Minecraft Mod Devloping. I love seeing all the funny things that I can do with just some simple lines of Java."
         },
         {
           id: "third-p-about",
           content:
-            "Lorem ipsum pariatur consectetur laboris occaecat nulla aliqua irure ad deserunt duis. Eiusmod nulla cupidatat labore sint sit aute dolore irure nostrud ut incididunt. Anim laborum reprehenderit labore magna ut dolore quis irure. Labore ea duis deserunt ullamco irure fugiat deserunt ut nisi ea minim proident. Nisi consectetur do non magna duis aliqua minim minim veniam. In occaecat minim qui consequat elit mollit consectetur non id tempor. Amet adipisicing occaecat tempor culpa quis est duis."
+            "One of my other interests is playing Piano. To me, it feels like coding just without a Computer."
         }
       ]
     };
